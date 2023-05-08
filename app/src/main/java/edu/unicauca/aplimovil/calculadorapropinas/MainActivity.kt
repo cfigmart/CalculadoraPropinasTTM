ba08956781c1b45f45642d0f80d7f91be870a622
Cristhian Figueroa <cfigmart@users.noreply.github.com>
2023-05-08 15:34:53 -0500

    Calculadora con Campos de texto para propina y valor de servicio. Botones personalizados y  con acciones.

--- app/src/main/java/edu/unicauca/aplimovil/calculadorapropinas/MainActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/calculadorapropinas/MainActivity.kt
@@ -1,9 +1,12 @@
 package edu.unicauca.aplimovil.calculadorapropinas
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,8 +15,11 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,9 +46,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PantallaPrincipalPropinas() {
-    var valorIngresado by remember {mutableStateOf("12")}
-    var cantidad = valorIngresado.toDoubleOrNull()?: 0.0
-    val propina = calcularPropina(cantidad)
+    var propinaIngresada by remember { mutableStateOf("") }
+    var valorIngresado by remember { mutableStateOf("12") }
+    var cantidad = valorIngresado.toDoubleOrNull() ?: 0.0
+
+    val porcentajePropina = propinaIngresada.toDoubleOrNull() ?: 0.0
+    val propina = calcularPropina(cantidad, porcentajePropina)
+
+    val focusManager = LocalFocusManager.current
 
     Column(
         modifier = Modifier.padding(32.dp),
@@ -54,33 +65,64 @@ fun PantallaPrincipalPropinas() {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(Modifier.height(16.dp))
-        CampoTextoValorServicio(valorIngresado, {valorIngresado = it})
+        //Este para agregar el valor de lo que voy a pagar
+        CampoTextoValorServicio(
+            label = R.string.costo_de_servicio,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down)}
+            ),
+            valor = valorIngresado,
+            siCambiaValor = { valorIngresado = it })
+        //Donde ingreso el porcentaje de la propina
+        CampoTextoValorServicio(
+            label = R.string.propina,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { focusManager.clearFocus() }),
+            valor = propinaIngresada,
+            siCambiaValor = { propinaIngresada = it}
+        )
         Spacer(Modifier.height(24.dp))
         Text(
-            text = stringResource(R.string.valor_propina, ""),
+            text = stringResource(R.string.valor_propina, propina),
             modifier = Modifier.align(Alignment.CenterHorizontally),
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold
         )
 
     }
-
 }
+
 
 @Composable
 fun CampoTextoValorServicio(
+    @StringRes label: Int,
+    keyboardOptions: KeyboardOptions,
+    keyboardActions: KeyboardActions,
     valor: String,
-    onValueChange: (String)-> Unit
+    siCambiaValor: (String)-> Unit,
+    modifier: Modifier = Modifier,
 ) {
 
 
     TextField(
-        label = { Text(stringResource(R.string.costo_de_servicio))},
+        //label = { Text(stringResource(R.string.costo_de_servicio))},
+        label = { Text(stringResource(label)) },
         value = valor,
-        onValueChange = onValueChange,
+        onValueChange = siCambiaValor,
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
+        //keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
 
     )
 }
